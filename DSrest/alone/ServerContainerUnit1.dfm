ee7cec6a0e9412649686e284ce7c4183ca78a707
dkrkdlqh <dkrkdlqh@hanmail.net>
2016-02-14 13:46:50 +0900

    ds_rest

---
+++ DSrest/alone/ServerContainerUnit1.dfm
@@ -0,0 +1,28 @@
+object ServerContainer1: TServerContainer1
+  OldCreateOrder = False
+  Height = 271
+  Width = 415
+  object DSServer1: TDSServer
+    Left = 96
+    Top = 11
+  end
+  object DSAuthenticationManager1: TDSAuthenticationManager
+    OnUserAuthenticate = DSAuthenticationManager1UserAuthenticate
+    OnUserAuthorize = DSAuthenticationManager1UserAuthorize
+    Roles = <
+      item
+        AuthorizedRoles.Strings = (
+          'admin')
+        ApplyTo.Strings = (
+          'echostring')
+      end>
+    Left = 96
+    Top = 197
+  end
+  object DSServerClass1: TDSServerClass
+    OnGetClass = DSServerClass1GetClass
+    Server = DSServer1
+    Left = 200
+    Top = 11
+  end
+end
---
+++ DSrest/mod_webbroker.dpr
@@ -0,0 +1,56 @@
+library mod_webbroker;
+
+{$R *.dres}
+
+uses
+  Winapi.ActiveX,
+  System.Win.ComObj,
+  Web.WebBroker,
+  Web.ApacheApp,
+  Web.HTTPD22Impl,
+  Data.DBXCommon,
+  Datasnap.DSSession,
+  ServerMethodsUnit1 in 'ServerMethodsUnit1.pas',
+  ServerContainerUnit1 in 'ServerContainerUnit1.pas' {ServerContainer1: TDataModule},
+  WebModuleUnit1 in 'WebModuleUnit1.pas' {WebModule1: TWebModule};
+
+{$R *.res}
+
+// httpd.conf entries:
+//
+(*
+ LoadModule webbroker_module modules/mod_webbroker.dll
+
+ <Location /xyz>
+    SetHandler mod_webbroker-handler
+ </Location>
+*)
+//
+// These entries assume that the output directory for this project is the apache/modules directory.
+//
+// httpd.conf entries should be different if the project is changed in these ways:
+//   1. The TApacheModuleData variable name is changed
+//   2. The project is renamed.
+//   3. The output directory is not the apache/modules directory
+//
+
+// Declare exported variable so that Apache can access this module.
+var
+  GModuleData: TApacheModuleData;
+exports
+  GModuleData name 'webbroker_module';
+
+procedure TerminateThreads;
+begin
+  TDSSessionManager.Instance.Free;
+  Data.DBXCommon.TDBXScheduler.Instance.Free;
+end;
+
+begin
+  CoInitFlags := COINIT_MULTITHREADED;
+  Web.ApacheApp.InitApplication(@GModuleData);
+  Application.Initialize;
+  Application.WebModuleClass := WebModuleClass;
+  TApacheApplication(Application).OnTerminate := TerminateThreads;
+  Application.Run;
+end.
